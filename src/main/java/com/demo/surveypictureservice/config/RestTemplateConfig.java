720035864a11d756c1a6824b9349d721bd5e777e
Steven <steven.itsme321@gmail.com>
2020-09-03 20:48:27 +0700

    Add rest template config

---
+++ src/main/java/com/demo/surveypictureservice/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.surveypictureservice.config;
+
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ *
+ * @author Steven Raylianto K.
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplateWithErrorHandler() {
+        return new RestTemplateBuilder().build();
+    }
+}
